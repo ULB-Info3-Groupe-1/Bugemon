82e2c78e65da3a1194fac72b5686b1388c369cc1
lulu <lucas.verbeiren@gmail.com>
2026-02-25 16:35:39 +0100

    doc(mainMenuController)
    Co-authored-by: Ethan <ethan.van.ruyskensvelde@ulb.be>

--- src/main/java/ulb/controllers/MainMenuController.java
+++ src/main/java/ulb/controllers/MainMenuController.java
@@ -5,13 +5,25 @@ import java.io.IOException;
 import ulb.controllers.MetaController.Window;
 import ulb.views.MainMenuView;
 
+/**
+ * MainMenuController
+ *
+ * Controller responsible for the main menu screen.
+ */
 public class MainMenuController extends Controller<MainMenuView> {
 
+    /**
+     * @param metaController
+     * @throws IOException
+     */
     public MainMenuController(MetaController metaController) throws IOException {
         super(metaController, new MainMenuView());
         this.view.setController(this);
     }
 
+    /**
+     * Callback invoked when the player wants to create a team.
+     */
     public void createTeam() {
         this.metaController.switchTo(Window.CREATE_TEAM);
     }
