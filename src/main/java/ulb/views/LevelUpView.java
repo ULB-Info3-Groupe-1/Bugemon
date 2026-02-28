eec3f4ae2321ec187a914be6c049fe07d63ad8bc
Romain Dejean <romain.dejean@ulb.be>
2026-02-28 19:59:53 +0100

    Added view and fxml of the level up
    Co-authored-by: Rareș Radu Loghin <rares.radu.loghin@ulb.be>

--- src/main/java/ulb/views/LevelUpView.java
+++ src/main/java/ulb/views/LevelUpView.java
@@ -2,6 +2,11 @@ package ulb.views;
 
 import java.io.IOException;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import ulb.controllers.LevelUpController;
 
 /**
@@ -11,13 +16,43 @@ import ulb.controllers.LevelUpController;
  */
 public class LevelUpView extends View {
 
+    @FXML
+    private Label TextLevelUp;
+    @FXML
+    private Button Stat1;
+    @FXML
+    private Button Stat2;
+    @FXML
+    private Button Stat3;
+    @FXML
+    private ImageView imageBugemon;
+
     private LevelUpController controller;
 
     public LevelUpView() throws IOException {
         super("/fxml/LevelUp.fxml");
         this.controller = null;
-    }
 
+        this.Stat1.setOnAction((e) -> this.controller.chooseOption(1));
+
+
+        this.Stat2.setOnAction((e) -> this.controller.chooseOption(2));
+
+
+        this.Stat3.setOnAction((e) -> this.controller.chooseOption(3));
+    }
+    public void setLevelUp() {
+        StringBuilder texte = new StringBuilder();
+        String filename = "ressources/Assets/png/" + LevelUpDTO.getBugemonName() + ".png";
+        Image image = new Image(filename);
+
+        texte.append(LevelUpDTO.getBugemonName());
+        texte.append(" a atteint le niveau ");
+        texte.append(LevelUpDTO.getNextLevel());
+
+        TextLevelUp.setText(texte.toString());
+        imageBugemon.setImage(image);
+    }
     /**
      * Binds Level up view to its controller.
      *
@@ -26,5 +61,6 @@ public class LevelUpView extends View {
     public void setController(LevelUpController controller) {
         this.controller = controller;
     }
+
 
 }
