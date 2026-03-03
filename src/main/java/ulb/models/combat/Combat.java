0cc46f17c016d2991d0cafa71f16f3121b728ab7
MANUEL ROCCA <manuel.rocca@ulb.be>
2026-03-03 16:46:46 +0100

    Restored yesterday's code

--- src/main/java/ulb/models/combat/Combat.java
+++ src/main/java/ulb/models/combat/Combat.java
@@ -9,18 +9,8 @@
 
 package ulb.models.combat;
 
-import java.io.IOException;
-import java.security.KeyException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import ulb.models.trainer.AutoTrainer;
 import ulb.models.trainer.Trainer;
-import ulb.models.bugemon.Attack;
-import ulb.models.bugemon.Bugemon;
-import ulb.models.bugemon.Effect;
-import ulb.models.bugemon_team.BugemonTeam;
+
 
 public class Combat {
 
@@ -29,9 +19,6 @@ public class Combat {
     protected Trainer allyTrainer; // allied trainer
     protected Trainer adversaryTrainer; // adversary trainer
     protected int turn;
-
-    protected Map<String, Map<Integer, Effect>> allyEffects = new HashMap<>();
-    protected Map<String, Effect> adversaryEffects = new HashMap<>();
 
     // Constructor
 
@@ -46,20 +33,6 @@ public class Combat {
     }
 
     // Methods
-
-    /**
-     * 
-     * @return
-     */
-    public Trainer turn() {
-        // applyAttack();
-        Trainer winner = getWinner();
-        if (winner != null) {
-            return winner;
-        }
-        nextTurn();
-        return null;
-    }
 
     /**
      * Returns the winner of the combat if it is over, null otherwise.
@@ -107,42 +80,5 @@ public class Combat {
      */
     public Trainer getAdversaryTrainer() {
         return this.adversaryTrainer;
-    }
-
-    protected void applyAttack(Trainer attacker, Trainer defender, Attack attack) throws KeyException {
-        List<Effect> effects = attack.getEffects();
-
-        for (Effect e : effects) {
-            String target = e.getTarget();
-
-            switch (target) {
-                case "adversaire":
-                    handleEffect(defender.getCurrentBugemon(), e);
-                    break;
-                case "lanceur":
-                    handleEffect(attacker.getCurrentBugemon(), e);
-                    break;
-                case "equipe":
-                    BugemonTeam team = attacker.getTeam();
-                    for (Bugemon bugemon : team.getTeam()) {
-                        handleEffect(bugemon, e);
-                    }
-                    break;
-                default:
-                    throw new KeyException("Invalid effect target");
-            }
-        }
-    }
-
-    private void handleEffect(Bugemon bugemon, Effect effect) {
-        int value = effect.getModifier();
-        String stat = effect.getStat();
-        try {
-            bugemon.editStat(stat, value);
-        } catch (Exception e) {
-            // TODO: handle exception -> Logger ?
-            System.err.println(e.getStackTrace());
-        }
-
     }
 }
