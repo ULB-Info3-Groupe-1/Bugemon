e6c074cdd193488a3053483601b44862c91a14a4
MANUEL ROCCA <manuel.rocca@ulb.be>
2026-03-04 04:33:50 +0100

    Added tests for the key of the edited stat for a bugemon

--- src/test/java/ulb/models/bugemon/TestBugemon.java
+++ src/test/java/ulb/models/bugemon/TestBugemon.java
@@ -11,6 +11,7 @@ package ulb.models.bugemon;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
