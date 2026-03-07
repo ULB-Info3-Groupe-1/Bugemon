758071f217978978093d2a67648eeeff03852ee9
Matteo Morbée <matteo.morbee@ulb.be>
2026-03-07 14:18:38 +0100

    fix: residual from rebase

--- src/test/java/ulb/models/bugemon_team/TestBugemonTeam.java
+++ src/test/java/ulb/models/bugemon_team/TestBugemonTeam.java
@@ -12,7 +12,6 @@ package ulb.models.bugemon_team;
 
 import static org.junit.Assert.*;
 
-import java.io.IOException;
 import java.io.InputStream;
 import org.junit.Test;
 import ulb.models.bugemon.Bugemon;
@@ -114,22 +113,6 @@ public class TestBugemonTeam {
         assertThrows(IllegalStateException.class, () -> {
             fullTeam.addBugemon(extraBugemon);
         });
-    }
-
-    private Parser.ParseResult loadResources() throws IOException {
-        try (
-            InputStream attacksStream = getClass().getResourceAsStream(
-                "/json/attaques.json"
-            );
-            InputStream bugemonsStream = getClass().getResourceAsStream(
-                "/json/bugemons.json"
-            );
-        ) {
-            if (attacksStream == null || bugemonsStream == null) {
-                throw new IOException("JSON files not found in resources: ");
-            }
-            return Parser.parse(attacksStream, bugemonsStream);
-        }
     }
 
     @Test
