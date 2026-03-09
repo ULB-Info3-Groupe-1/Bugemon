675131df25d68c63a9d465ad228e14edc873c7bf
Ethan Van Ruyskensvelde <ethanvr3@gmail.com>
2026-03-09 17:50:49 +0100

    feat(Change GUI text to french)

--- src/main/java/ulb/views/combat/MainActionMenu.java
+++ src/main/java/ulb/views/combat/MainActionMenu.java
@@ -6,13 +6,13 @@ import ulb.views.ActionMenuView;
 public class MainActionMenu extends ActionMenuView {
     public MainActionMenu() {
         super();
-        this.action1.setText("Attack");
+        this.action1.setText("Attaque");
         this.action1.getStyleClass().add("action-attack");
-        this.action2.setText("Switch");
+        this.action2.setText("Changer de Bugémon");
         this.action2.getStyleClass().add("action-switch");
-        this.action3.setText("Use Item");
+        this.action3.setText("Utiliser un objet");
         this.action3.getStyleClass().add("action-item");
-        this.action4.setText("Surrender");
+        this.action4.setText("Abandonner");
     }
 
     /**
