d74950cfe6a706d7f548dbcf57b4afb023b8da24
Ethan Van Ruyskensvelde <ethanvr3@gmail.com>
2026-03-09 10:06:18 +0100

    add missing package info for root ulb entry

---
+++ src/main/java/ulb/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Root package of the Bugémon application.
+ *
+ * <p>This package contains the main entry points of the game,
+ * including the application launcher and the main class.
+ * The Bugémon project is a Pokémon-inspired game developed
+ * as part of a ULB project.</p>
+ */
+package ulb;
