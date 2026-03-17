ecaff425d8eed91c7c58130d52467a91b1e5900b
Martin Gouverneur <gouverneur.martin@hotmail.com>
2026-03-17 18:00:57 +0100

    feat(repository): initial structure and basic queries
    Co-authored-by: Ethan <ethan.van.ruyskensvelde@ulb.be>

---
+++ src/main/resources/sql/03_queries_user_bugemons.sql
@@ -0,0 +1,19 @@
+-- Query
+-- SaveUserBugemon
+INSERT INTO user_bugemons
+  (user_id, bugemon_id, current_defense, current_attack_power,
+   current_initiative, current_max_hp, current_xp, current_level)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+ON CONFLICT (user_id, bugemon_id) DO NOTHING;
+
+-- Query
+-- UpdateUserBugemon
+UPDATE user_bugemons
+SET current_defense = ?, current_attack_power = ?,
+    current_initiative = ?, current_max_hp = ?,
+    current_xp = ?, current_level = ?
+WHERE user_id = ? AND bugemon_id = ?;
+
+-- Query
+-- GetUserBugemons
+SELECT * FROM user_bugemons WHERE user_id = ?;
