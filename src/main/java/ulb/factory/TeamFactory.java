0df247ae7e9e723c82a2d58ac23ce79cc3b0be9d
MANUEL ROCCA <manuel.rocca@ulb.be>
2026-03-18 17:11:01 +0100

    Changed the bugemon list argument to a static final attribute of the TeamFactory class and added createBossTeam method
    Co-authored-by: Matteo <matteo.morbee@ulb.be>

--- src/main/java/ulb/factory/TeamFactory.java
+++ src/main/java/ulb/factory/TeamFactory.java
@@ -3,12 +3,19 @@ package ulb.factory;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
+import java.util.Optional;
 
 import ulb.models.bugemon.Bugemon;
 import ulb.models.bugemon_team.BugemonTeam;
 import ulb.utils.Parser;
+import ulb.utils.Parser;
 
 public class TeamFactory {
+    private static final Random RANDOM = new Random();
+    private static final List<Bugemon> BUGEMON_LIST = Parser.getInstance().getBugemons();
+    private static final String BOSS_ID = "finalboss";
+
     /**
      * Generates a random {@link BugemonTeam} of the specified size by sampling
      * without replacement from the given pool of available {@link Bugemon}s.
@@ -29,16 +36,33 @@ public class TeamFactory {
      *         chosen, cloned {@link Bugemon}s.
      * @throws RuntimeException if cloning a selected {@link Bugemon} fails.
      */
-    public static BugemonTeam createRandomTeam(final List<Bugemon> bugemonList,
-                                               final int teamSize) {
-        List<Bugemon> allBugemons = Parser.getInstance().getBugemons();
-        List<Bugemon> pool = new ArrayList<>(allBugemons);
-        Collections.shuffle(pool);
-        BugemonTeam team = new BugemonTeam();
-        for (int i = 0; i < teamSize; i++) {
-            team.add(pool.get(i).clone());
-        }
+    public static BugemonTeam createRandomTeam(final int teamSize) {
+        BugemonTeam randomTeam = new BugemonTeam();
 
-        return team;
+        while (randomTeam.size() != teamSize) {
+            int randomIndex = RANDOM.nextInt(BUGEMON_LIST.size());
+            Bugemon chosenBugemon = BUGEMON_LIST.get(randomIndex);
+
+            if (!randomTeam.contains(chosenBugemon.getId())) {
+                try {
+                    randomTeam.addBugemon(chosenBugemon.clone());
+                } catch (CloneNotSupportedException e) {
+                    throw new RuntimeException("Failed to clone Bugemon: " + chosenBugemon.getId(),
+                            e);
+                }
+            }
+        }
+        return randomTeam;
+    }
+
+    public static BugemonTeam createBossTeam() {
+
+        final Optional<Bugemon> bossBugemon = BUGEMON_LIST.stream().filter(obj -> obj.getId().equals(BOSS_ID))
+                .findFirst();
+        BugemonTeam bossTeam = new BugemonTeam();
+
+        bossTeam.add(bossBugemon.orElseThrow());
+
+        return bossTeam;
     }
 }
