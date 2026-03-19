f762572d4dfc0a025c1154484e191e5d6e5b510e
Ethan Van Ruyskensvelde <ethanvr3@gmail.com>
2026-03-19 12:36:49 +0100

    feat(db): fix database initialization and test setup
    Add .env file to store database connection details and update .gitlab-ci.yml to use these details for CI/CD pipeline. Modify DatabaseRepository to check for the existence of critical tables and insert static game data if necessary. Update TestRepository to ensure a clean state before each test by clearing the database and re-inserting static game data.
    
    Co-authored-by: Martin <martin.gouverneur@ulb.be>

--- src/main/resources/sql/check_db_status.sql
+++ src/main/resources/sql/check_db_status.sql
@@ -10,10 +10,7 @@ SELECT
 
 -- Query
 -- isTablesPresent
-SELECT 
-    (SELECT COUNT(*) FROM pg_tables WHERE schemaname = 'public') as total_tables,
-    (SELECT COUNT(*) 
-     FROM information_schema.tables 
-     WHERE table_schema = 'public' 
-     AND table_name IN ('attacks', 'effects', 'bugemons', 'users', 'user_bugemons', 'teams', 'team_members')
-    ) as existing_critical_tables;
+SELECT COUNT(*) as existing_critical_tables
+FROM pg_tables
+WHERE schemaname = 'public'
+AND tablename IN ('bugemons', 'attacks', 'effects', 'users', 'user_bugemons', 'teams', 'team_members');
