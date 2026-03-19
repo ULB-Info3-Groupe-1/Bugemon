6492783ad922a67ecc768018ba0c2943a3131cd4
Philippe Brisbois <philippe.brisbois@ulb.be>
2026-03-20 00:06:17 +0100

    Deleted EffectType
    Co-authored-by: Ethan Van Ruyskensvelde<ethan.van.ruyskensvelde@ulb.be>

--- src/main/java/ulb/models/bugemon/effect/EffectType.java
+++
@@ -1,9 +0,0 @@
-package ulb.models.bugemon.effect;
-
-import com.google.gson.annotations.SerializedName;
-
-public enum EffectType {
-    @SerializedName("stat_modifier") STAT_MODIFIER,
-    @SerializedName("reset_malus") RESET_PENALTY,
-    @SerializedName("soin") HEAL,
-}
