c223a65b2449c061408c2db4e6028fe73a7a3084
romainlief <romain.liefferinckx@ulb.be>
2026-03-27 00:17:40 +0100

    formatage

--- src/main/java/ulb/models/bugemon/Bugemon.java
+++ src/main/java/ulb/models/bugemon/Bugemon.java
@@ -332,14 +332,12 @@ public class Bugemon implements BugemonDTO {
 
     public void addEffect(Effect effect) {
         switch (effect) {
-            case EffectStatModifier e
-                    -> {
-
+            case EffectStatModifier e -> {
                 // TODO: I feel like this part should probably be done elsewhere
                 EffectDuration duration = e.duration();
-                Modifier modifier =
-                        (duration == EffectDuration.ONE_TURN) ? new Modifier(e.modifier(), 1):
-                new Modifier(e.modifier());
+                Modifier modifier = (duration == EffectDuration.ONE_TURN)
+                                            ? new Modifier(e.modifier(), 1)
+                                            : new Modifier(e.modifier());
 
                 switch (e.stat()) {
                     case EffectStat.HP:
@@ -355,7 +353,7 @@ public class Bugemon implements BugemonDTO {
                         this.initiativeComponent.addModifier(modifier);
                         break;
                 }
-        }
+            }
 
             case EffectHeal e -> {
                 this.healthComponent.increaseHp(e.amount());
