6cab67e76fd18278fecb7cb6e90518fdf7fbcdbb
Romain Dejean <romain.dejean@ulb.be>
2026-03-27 22:38:37 +0100

    fix(inventoryTest) : fixed tests

--- src/test/java/ulb/services/TestInventoryService.java
+++ src/test/java/ulb/services/TestInventoryService.java
@@ -8,8 +8,7 @@ import org.junit.Test;
 
 import ulb.models.bugemon.Inventory;
 import ulb.models.bugemon.Item;
-import ulb.models.bugemon.effect.EffectHeal;
-import ulb.models.bugemon.effect.EffectTarget;
+import ulb.models.bugemon.effect.*;
 
 public class TestInventoryService {
     private Inventory expectedInventory;
@@ -31,10 +30,14 @@ public class TestInventoryService {
                                Item.ItemType.HEALING, new EffectHeal(EffectTarget.THROWER, 10));
         gelDefensif = new Item("gel_defensif", "Gel Defensif",
                                "Renforce temporairement la defense du Bugémon actif.",
-                               Item.ItemType.BOOST, null);
-        serumOffensif = new Item("serum_offensif", "Serum Offensif",
-                                 "Renforce temporairement l'attaque du Bugémon actif.",
-                                 Item.ItemType.BOOST, null);
+                               Item.ItemType.BOOST,
+                               new EffectStatModifier(EffectTarget.THROWER, EffectStat.DEFENSE, 10,
+                                                      EffectDuration.PERMANENT));
+        serumOffensif =
+                new Item("serum_offensif", "Serum Offensif",
+                         "Renforce temporairement l'attaque du Bugémon actif.", Item.ItemType.BOOST,
+                         new EffectStatModifier(EffectTarget.THROWER, EffectStat.ATTACK, 10,
+                                                EffectDuration.PERMANENT));
 
         expectedInventory.addItem(baieRevigorante, 3);
         expectedInventory.addItem(baieTonique, 2);
