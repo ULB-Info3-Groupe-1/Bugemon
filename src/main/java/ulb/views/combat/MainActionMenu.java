e7127d42d0720faeabbc7e6db38ba03d738a1d67
Romain Dejean <romain.dejean@ulb.be>
2026-03-27 22:18:03 +0100

    feat(itemUsage) : added view and control of the items
    Co-authored-by: Philippe Brisbois <philippe.brisbois@ulb.be>

--- src/main/java/ulb/views/combat/MainActionMenu.java
+++ src/main/java/ulb/views/combat/MainActionMenu.java
@@ -51,4 +51,11 @@ public class MainActionMenu extends ActionMenuView {
                 callback.run();
         });
     }
+    public void setOnInventory(Runnable callback){
+        this.action3.setOnAction(e -> {
+            if (callback != null)
+                callback.run();
+        });
+    }
 }
+
