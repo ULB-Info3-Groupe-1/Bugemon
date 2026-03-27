bae6d33f6d4adceebb9278afa632d247316456a1
Romain Dejean <romain.dejean@ulb.be>
2026-03-27 22:18:03 +0100

    feat(itemUsage) : added view and control of the items
    Co-authored-by: Philippe Brisbois <philippe.brisbois@ulb.be>

--- src/main/java/ulb/controllers/combat/ManualCombatController.java
+++ src/main/java/ulb/controllers/combat/ManualCombatController.java
@@ -177,7 +177,5 @@ public class ManualCombatController extends CombatController<ManualCombatView> {
         this.playerTrainer.registerUseItem(item);
         this.combat.turn();
         this.view.refresh();
-
-
     }
 }
