f7a4af196c27fa8723a46def058fb6796bc04228
Arkham591 <rraduloghon@gmail.com>
2026-04-02 23:12:11 +0200

    refactor of ObjectFix
    Co-authored-by: Lucas <lucas.verbeiren@ulb.be>

--- src/main/java/ulb/controllers/combat/ManualCombatController.java
+++ src/main/java/ulb/controllers/combat/ManualCombatController.java
@@ -175,6 +175,6 @@ public class ManualCombatController extends CombatController<ManualCombatView> {
         this.playerTrainer.registerUseItem(item);
         this.combat.turn();
         this.view.refresh();
-        handlePostTurn(this.combat.turn());
+        this.handlePostTurn(this.combat.turn());
     }
 }
