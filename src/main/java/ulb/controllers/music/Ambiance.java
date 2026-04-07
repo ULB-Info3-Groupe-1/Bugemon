c7f2b0f871172668bba62476ee759d7d12b878f9
lulu <lucas.verbeiren@gmail.com>
2026-04-07 22:07:52 +0200

    formatting with new formatter config

--- src/main/java/ulb/controllers/music/Ambiance.java
+++ src/main/java/ulb/controllers/music/Ambiance.java
@@ -4,5 +4,9 @@ package ulb.controllers.music;
  * Enumerates the different musical ambiances available in the game.
  */
 public enum Ambiance {
-    COMBAT, MENU, CREATE_TEAM, VICTORY, DEFEAT
+    COMBAT,
+    MENU,
+    CREATE_TEAM,
+    VICTORY,
+    DEFEAT
 }
--- src/main/java/ulb/models/bugemon/effect/EffectStat.java
+++ src/main/java/ulb/models/bugemon/effect/EffectStat.java
@@ -4,8 +4,11 @@ import com.google.gson.annotations.SerializedName;
 
 public enum EffectStat {
     @SerializedName("pv")
-    HP, @SerializedName("attaque")
-    ATTACK, @SerializedName("defense")
-    DEFENSE, @SerializedName("initiative")
+    HP,
+    @SerializedName("attaque")
+    ATTACK,
+    @SerializedName("defense")
+    DEFENSE,
+    @SerializedName("initiative")
     INITIATIVE,
 }
