12a5c247213be515039d5ebf3e95af7277966a28
Ethan Van Ruyskensvelde <ethanvr3@gmail.com>
2026-04-07 12:06:07 +0200

    refactor(repository): move getSql function in abstract class
    Co-authored-by: lucas <lucas.verbeiren@ulb.be>

---
+++ src/main/java/ulb/repository/AbstractRepository.java
@@ -0,0 +1,26 @@
+package ulb.repository;
+
+import java.util.Map;
+
+public abstract class AbstractRepository {
+
+    private final Map<String, String> queries;
+
+    protected AbstractRepository(Map<String, String> queries) {
+        this.queries = queries;
+    }
+
+    /**
+     * Returns the SQL string for the given query name.
+     *
+     * @throws IllegalArgumentException
+     *             if the query name is not found
+     */
+    protected String getSql(String queryName) {
+        String sql = this.queries.get(queryName);
+        if (sql == null) {
+            throw new IllegalArgumentException("SQL query not found in Map : " + queryName);
+        }
+        return sql;
+    }
+}
