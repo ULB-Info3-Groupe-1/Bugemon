9e8ca0c419b3c7afb25da82bf73eafd700c91bba
lulu <lucas.verbeiren@gmail.com>
2026-04-11 13:22:44 +0200

    bugemon apply effect more dynamic: avoid switch cases on types
    Co-authored-by: Rareș Radu Loghin <rares.radu.loghin@ulb.be>

--- src/main/java/ulb/models/bugemon/effect/EffectResetMalus.java
+++ src/main/java/ulb/models/bugemon/effect/EffectResetMalus.java
@@ -1,4 +1,10 @@
 package ulb.models.bugemon.effect;
 
+import ulb.models.bugemon.Bugemon;
+
 public record EffectResetMalus(EffectTarget target) implements Effect {
+    @Override
+    public void applyTo(Bugemon bugemon) {
+        bugemon.apply(this);
+    }
 }
