5ffce445add00e34abb4998a74546e5ebbe82299
Ethan Van Ruyskensvelde <ethanvr3@gmail.com>
2026-04-20 19:37:02 +0200

    remove playerId and add current team and tower level to database

--- src/main/java/ulb/repositories/dto/PlayerBugemonDTO.java
+++ src/main/java/ulb/repositories/dto/PlayerBugemonDTO.java
@@ -1,5 +1,5 @@
 package ulb.repositories.dto;
 
-public record PlayerBugemonDTO(int playerId, String bugemonName, int currentDefense, int currentAttackPower,
+public record PlayerBugemonDTO(String playername, String bugemonName, int currentDefense, int currentAttackPower,
         int currentInitiative, int currentMaxHp, int currentXp, int currentLevel) {
 }
--- src/main/resources/sql/queries_teams.sql
+++ src/main/resources/sql/queries_teams.sql
@@ -1,49 +1,49 @@
 -- Query
 -- CreateTeam
-INSERT INTO teams (player_id, name)
+INSERT INTO teams (playername, name)
 VALUES (?, ?)
-ON CONFLICT (player_id, name) DO NOTHING;
+ON CONFLICT (playername, name) DO NOTHING;
 
 -- Query
 -- DeleteTeamMembers
-DELETE FROM team_members WHERE player_id = ? AND team_name = ?;
+DELETE FROM team_members WHERE playername = ? AND team_name = ?;
 
 -- Query
 -- DeleteTeam
-DELETE FROM teams WHERE player_id = ? AND name = ?;
+DELETE FROM teams WHERE playername = ? AND name = ?;
 
 -- Query
 -- GetPlayerTeams
-SELECT * FROM teams WHERE player_id = ?;
+SELECT * FROM teams WHERE playername = ?;
 
 -- Query
 -- AddTeamMember
-INSERT INTO team_members (player_id, team_name, bugemon_name, slot_position)
+INSERT INTO team_members (playername, team_name, bugemon_name, slot_position)
 VALUES (?, ?, ?, ?)
-ON CONFLICT (player_id, team_name, slot_position) DO NOTHING;
+ON CONFLICT (playername, team_name, slot_position) DO NOTHING;
 
 -- Query
 -- RemoveTeamMember
 DELETE FROM team_members
-WHERE player_id = ? AND team_name = ? AND bugemon_name = ?;
+WHERE playername = ? AND team_name = ? AND bugemon_name = ?;
 
 -- Query
 -- GetTeamMembers
-SELECT * FROM team_members WHERE player_id = ? AND team_name = ?;
+SELECT * FROM team_members WHERE playername = ? AND team_name = ?;
 
 -- Query
 -- RenameTeam
-UPDATE teams SET name = ? WHERE player_id = ? AND name = ?;
+UPDATE teams SET name = ? WHERE playername = ? AND name = ?;
 
 -- Query
 -- RenameTeamMembers
-UPDATE team_members SET team_name = ? WHERE player_id = ? AND team_name = ?;
+UPDATE team_members SET team_name = ? WHERE playername = ? AND team_name = ?;
 
 -- Query
 -- RemoveTeamComposition
 DELETE FROM team_members 
-WHERE player_id = ? AND team_name = ?;
+WHERE playername = ? AND team_name = ?;
 
 -- Query
 -- TeamNameAlreadyExists
-SELECT * FROM teams WHERE player_id = ? AND name = ?;
+SELECT * FROM teams WHERE playername = ? AND name = ?;
