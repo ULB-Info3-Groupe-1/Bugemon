696e02b62f34a6bf3975928b02bd819c1cea96d4
Ethan Van Ruyskensvelde <ethanvr3@gmail.com>
2026-04-22 11:19:39 +0200

    fix tower and tower test

--- src/test/java/ulb/models/tower/TestTower.java
+++ src/test/java/ulb/models/tower/TestTower.java
@@ -58,22 +58,23 @@ public class TestTower {
     public void testNOTowerInitialization() {
         BugemonTeam playerTeam = TestUtilsBugemons.createDefaultTeam(3);
 
+        // No tower structure with floor NO2, NO3, NO4, NO5, NO6, NO7, NO8
         Tower noTower = new Tower(playerTeam, INVENTORY_SERVICE_MOCK.getInventory(), BUGEMON_SERVICE_MOCK, 2);
 
         assertEquals(2, noTower.getCurrentFloorNumber());
         assertFalse(noTower.isFloorComplete());
 
-        for (int expectedFloor = 3; expectedFloor <= 8; expectedFloor++) {
+        for (int expectedFloor = 2; expectedFloor <= 7; expectedFloor++) {
+            assertEquals(expectedFloor, noTower.getCurrentFloorNumber());
             this.completeCurrentFloor(noTower);
             assertTrue(noTower.isFloorComplete());
             noTower.goToNextFloor();
-            assertEquals(expectedFloor, noTower.getCurrentFloorNumber());
         }
 
+        assertEquals(8, noTower.getCurrentFloorNumber());
         this.completeCurrentFloor(noTower);
         assertTrue(noTower.isFloorComplete());
         assertThrows(IllegalStateException.class, noTower::goToNextFloor);
-        assertEquals(8, noTower.getCurrentFloorNumber());
     }
 
     @Test
