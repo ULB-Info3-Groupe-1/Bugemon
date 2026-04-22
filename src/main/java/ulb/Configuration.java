bd21a5ab91951c629c71ff69a9b4997c70bb22a0
Ethan Van Ruyskensvelde <ethanvr3@gmail.com>
2026-04-22 11:19:39 +0200

    fix tower and tower test

--- src/main/java/ulb/Configuration.java
+++ src/main/java/ulb/Configuration.java
@@ -77,6 +77,8 @@ public abstract class Configuration {
         public static final int MAX_TEAM_SIZE = 6;
         public static final String DEFAULT_TEAM_NAME = "Unnamed Team";
         public static final String BOSS_NAME = "FinalBoss";
+        public static final int FLOOR_MAX = 8; // The highest floor is NO8
+        public static final int FLOOR_MIN = 2; // The lowest floor is NO2
 
     }
 
