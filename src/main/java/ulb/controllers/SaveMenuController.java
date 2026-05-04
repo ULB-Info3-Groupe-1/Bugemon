b4c5b7a3798352a81c76a828301f4d2f59fc72ec
Ethan Van Ruyskensvelde <ethanvr3@gmail.com>
2026-05-04 14:23:26 +0200

    fix rebase development

--- src/main/java/ulb/controllers/SaveMenuController.java
+++ src/main/java/ulb/controllers/SaveMenuController.java
@@ -1,6 +1,7 @@
 package ulb.controllers;
 
 import ulb.services.BugemonService;
+import ulb.services.InventoryService;
 import ulb.services.TeamService;
 import ulb.services.TowerService;
 import ulb.views.SaveMenuView;
@@ -9,16 +10,18 @@ import ulb.views.ViewLoader;
 /** Controller for the main menu screen. */
 public class SaveMenuController extends Controller<SaveMenuView> implements SaveMenuView.Listener {
 
-    BugemonService bugemonService;
-    TeamService teamService;
-    TowerService towerService;
+    private final BugemonService bugemonService;
+    private final TeamService teamService;
+    private final TowerService towerService;
+    private final InventoryService inventoryService;
 
     public SaveMenuController(MetaController metaController, BugemonService bugemonService, TeamService teamService,
-            TowerService towerService) {
+            TowerService towerService, InventoryService inventoryService) {
         super(metaController, ViewLoader.load(SaveMenuView::new));
         this.bugemonService = bugemonService;
         this.teamService = teamService;
         this.towerService = towerService;
+        this.inventoryService = inventoryService;
         this.view.setListener(this);
     }
 
@@ -27,6 +30,7 @@ public class SaveMenuController extends Controller<SaveMenuView> implements Save
         this.bugemonService.clearAllPlayerBugemons();
         this.teamService.clearTeamsAndActiveTeam();
         this.towerService.clearTowerProgress();
+        this.inventoryService.loadInventory(); // TODO: check if needed or recreate a new inventory ?
         this.metaController.onMainMenu();
     }
 
@@ -34,6 +38,7 @@ public class SaveMenuController extends Controller<SaveMenuView> implements Save
     public void onContinue() {
         this.teamService.loadTeamsAndActiveTeam();
         this.towerService.loadTowerProgress();
+        this.inventoryService.loadInventory();
         this.metaController.onMainMenu();
     }
 
