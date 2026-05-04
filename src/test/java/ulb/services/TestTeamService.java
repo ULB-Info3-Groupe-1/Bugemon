cac15f2bb285ef407a5061ee8db81847a596edaf
Ethan Van Ruyskensvelde <ethanvr3@gmail.com>
2026-05-04 20:27:27 +0200

    add tests for services

--- src/test/java/ulb/services/TestTeamService.java
+++ src/test/java/ulb/services/TestTeamService.java
@@ -68,6 +68,7 @@ public class TestTeamService {
         this.teamService.addOrRemoveBugemon(bugemon);
         String teamName = "NewTeam";
         this.teamService.saveTeam(teamName);
+        this.teamService.setActiveTeam(teamName);
 
         verify(this.playerRepository).createTeam(PLAYER_NAME, teamName);
         assertEquals(teamName, this.teamService.getActiveTeam().get().getName());
