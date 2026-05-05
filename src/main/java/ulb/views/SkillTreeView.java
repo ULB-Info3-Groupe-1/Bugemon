a250f0363e97a8fab10af63d33426687eeded886
Philippe Brisbois <philippe.brisbois@ulb.be>
2026-05-05 14:03:52 +0200

    feat(skill-tree-view): add rendering in SkillTreeView
    Co-Authored-By: Lucas Verbeiren <lucas.verbeiren@ulb.be>

--- src/main/java/ulb/views/SkillTreeView.java
+++ src/main/java/ulb/views/SkillTreeView.java
@@ -1,15 +1,14 @@
 package ulb.views;
 
-// import java.io.File;
-// import javafx.fxml.FXML;
-// import javafx.scene.control.Button;
-// import javafx.scene.control.Label;
-// import javafx.scene.image.Image;
-// import javafx.scene.image.ImageView;
-
 import ulb.Configuration;
-// import ulb.common.dto.BugemonDTO;
-// import ulb.models.level_up.LevelUp;
+import ulb.views.utils.Node;
+import ulb.views.utils.TreeLayout;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
 
 public class SkillTreeView extends View {
     // Skill:
@@ -17,6 +16,19 @@ public class SkillTreeView extends View {
     // Use an algorithm to dispatch properly the node...
     // Node should have colors and unlocked or not...
     // Drawing the link between node ? Spline or straight line ?? Reingold–Tilford layout
+
+    private static final double NODE_WIDTH = 160.0;
+    private static final double NODE_HEIGHT = 100.0;
+    private static final double HORIZONTAL_SPACING = 100.0;
+    private static final double VERTICAL_SPACING = 100.0;
+
+    @FXML
+    private StackPane mapContainer;
+
+    @FXML
+    private Pane innerMapPane;
+
+    private Listener listener;
 
     @Override
     public String getPath() {
@@ -27,4 +39,58 @@ public class SkillTreeView extends View {
     public void refresh() {
 
     }
+
+    public void setListener(Listener listener) {
+        this.listener = listener;
+    }
+
+    @FXML
+    private void onReturnToMainMenuClicked() {
+        if (this.listener != null) {
+            this.listener.onReturnToMainMenu();
+        }
+    }
+
+    public void renderTree(Node root) {
+        TreeLayout.applyLayout(root);
+        this.renderNode(root);
+    }
+
+    private void renderNode(Node node) {
+        double pixelX = node.x * (NODE_WIDTH + HORIZONTAL_SPACING);
+        double pixelY = node.y * (NODE_HEIGHT + VERTICAL_SPACING);
+
+        // seen as a circle
+        HBox skillBox = new HBox();
+        skillBox.getStyleClass().add("hover-info-panel");
+
+        // adding data
+        Label label = new Label(node.getData());
+        label.getStyleClass().add("section-label");
+
+        // placing it properly
+        StackPane skillNode = new StackPane(skillBox, label);
+        skillNode.setPrefSize(NODE_WIDTH, NODE_HEIGHT);
+        skillNode.setLayoutX(pixelX);
+        skillNode.setLayoutY(pixelY);
+        skillNode.getStyleClass().add("skill-node");
+        skillNode.setOnMouseClicked(e -> {
+            if (this.listener != null) {
+                this.listener.onSkillClicked(node);
+            }
+        });
+
+        this.innerMapPane.getChildren().add(skillNode);
+
+        for (Node child : node.getChildren()) {
+            renderNode(child);
+        }
+    }
+
+    public interface Listener {
+        void onSkillClicked(Node node);
+
+        void onReturnToMainMenu();
+    }
+
 }
