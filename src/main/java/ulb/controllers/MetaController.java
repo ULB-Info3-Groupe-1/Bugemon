b256094b17ab68c8ebadebdf2b98922ee5de0a4c
Arkham591 <rraduloghon@gmail.com>
2026-05-05 16:06:21 +0200

    Fix 2: solved soundEffect problem
    Co-authored-by: Lucas <lucas.verbeiren@ulb.be>

--- src/main/java/ulb/controllers/MetaController.java
+++ src/main/java/ulb/controllers/MetaController.java
@@ -197,11 +197,11 @@ public class MetaController {
         });
         this.transitions.put(Window.COMBAT_VICTORY, () -> {
             this.combatVictoryController.show();
-            this.musicPlayer.playAmbiance(Ambiance.VICTORY, false);
+            this.musicPlayer.playAmbiance(Ambiance.VICTORY, true);
         });
         this.transitions.put(Window.COMBAT_DEFEAT, () -> {
             this.combatDefeatController.show();
-            this.musicPlayer.playAmbiance(Ambiance.DEFEAT, false);
+            this.musicPlayer.playAmbiance(Ambiance.DEFEAT, true);
         });
         this.transitions.put(Window.LEVEL_UP, this.levelUpController::show);
     }
