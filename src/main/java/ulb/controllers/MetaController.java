84aac40abe09a9abc1ed90a6bf31dd0231a07080
lulu <lucas.verbeiren@gmail.com>
2026-05-09 11:45:39 +0200

    TowerController: runTower -> show
    Co-authored-by: Ethan <ethan.van.ruyskensvelde@ulb.be>

--- src/main/java/ulb/controllers/MetaController.java
+++ src/main/java/ulb/controllers/MetaController.java
@@ -208,7 +208,7 @@ public class MetaController {
         this.transitions.put(Window.NOTOWER, () -> {
             this.isTowerActive = true;
             this.musicPlayer.playAmbiance(Ambiance.COMBAT, false);
-            this.towerController.runTower();
+            this.towerController.show();
         });
         this.transitions.put(Window.COMBAT_VICTORY, () -> {
             this.combatVictoryController.show();
