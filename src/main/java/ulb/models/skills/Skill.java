13ae8149a441e13eb094c048f1c3365d24424480
MANUEL ROCCA <manuel.rocca@ulb.be>
2026-05-09 15:25:43 +0200

    Added skill prerequisite checking in skillnode

--- src/main/java/ulb/models/skills/Skill.java
+++ src/main/java/ulb/models/skills/Skill.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import ulb.models.bugemon.effect.Effect;
 
+/**
+ * Represents a skill that can be unlocked in the skill tree.
+ */
 public class Skill {
 
     private String id;
@@ -16,9 +19,7 @@ public class Skill {
 
     private int maxLevel;
 
-    private Effect effect; // TODO: is it the right effect ?
-
-    private List<String> prerequisites; // List of skill IDs that must be unlocked before this skill can be unlocked
+    private Effect effect;
 
     private boolean isUnlocked;
 
@@ -30,18 +31,34 @@ public class Skill {
         this.cost = cost;
         this.maxLevel = maxLevel;
         this.effect = effect;
-        this.prerequisites = prerequisites;
     }
 
-    public List<String> getPrerequisites() {
-        return this.prerequisites;
+    public String getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public int getCost() {
+        return this.cost;
+    }
+
+    public int getMaxLevel() {
+        return this.maxLevel;
+    }
+
+    public Effect getEffect() {
+        return this.effect;
     }
 
     public boolean isUnlocked() {
         return this.isUnlocked;
     }
 
-    public String getId() {
-        return this.id;
-    }
 }
