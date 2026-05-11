62440bd52779a25f1ac91d6fed1ad853ee65ab39
MANUEL ROCCA <manuel.rocca@ulb.be>
2026-05-12 00:39:18 +0200

    Working attack change and stat update
    Co-authored-by: Romain Dejean <romain.dejean@ulb.be>

--- src/main/java/ulb/controllers/RewardController.java
+++ src/main/java/ulb/controllers/RewardController.java
@@ -1,12 +1,17 @@
 package ulb.controllers;
 
+import static org.junit.jupiter.api.DynamicTest.stream;
+
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import ulb.models.bugemon.Attack;
 import ulb.models.bugemon.Bugemon;
+import ulb.models.bugemon.BugemonType;
 import ulb.models.bugemon_team.BugemonTeam;
+import ulb.models.reward.AttackReward;
 import ulb.models.reward.Reward;
 import ulb.services.BugemonService;
 import ulb.services.RewardService;
@@ -70,9 +75,41 @@ public class RewardController extends Controller<RewardView> implements RewardVi
         if (this.pendingReward == null) {
             throw new IllegalStateException("Impossible to assign a null reward to" + bugemon.getName());
         }
-        this.pendingReward.applyReward(bugemon);
+
+        // TODO (just to be found)
+        // disclaimer: WE MUST USE THE SWITCH CASE IN ORDER TO UPDATE THE VIEW PROPERLY
+        // hihi stinky poopy
+
+        switch (this.pendingReward.getRewardType()) {
+            case ATTACK -> {
+                this.updateAttackSelection(bugemon);
+                break;
+            }
+            default -> {
+                this.pendingReward.applyReward(bugemon);
+                this.bugemonService.saveBugemonState(bugemon);
+                this.quitRewardScreen();
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onAttackChosen(Bugemon bugemon, int attackIndex) {
+
+        AttackReward attackReward = (AttackReward) this.pendingReward;
+        attackReward.setIndexToReplace(attackIndex);
+        attackReward.applyReward(bugemon);
         this.bugemonService.saveBugemonState(bugemon);
         this.quitRewardScreen();
+    }
+
+    private void updateAttackSelection(Bugemon bugemon) {
+        List<String> updatedAttacks = bugemon.getAttackList()
+                .stream()
+                .map(Attack::name)
+                .toList();
+        this.view.setAttackList(updatedAttacks);
     }
 
     private void quitRewardScreen() {
