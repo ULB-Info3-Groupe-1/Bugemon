e8895f5cc1470f9bd2ee70b5d4033ed000b602a5
lulu <lucas.verbeiren@gmail.com>
2026-05-12 13:42:00 +0200

    bugemon record
    Co-authored-by: Matteo Morbée <matteo.morbee@ulb.be>

--- src/main/java/ulb/models/bugemon/Bugemon.java
+++ src/main/java/ulb/models/bugemon/Bugemon.java
@@ -1,6 +1,5 @@
 package ulb.models.bugemon;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
@@ -10,88 +9,40 @@ import ulb.models.bugemon.exceptions.InvalidAttackCountException;
 
 /**
  * Represents a Bugemon.
- */ 
-public class Bugemon {
+ */
+public record Bugemon(
+        String id,
+        String name,
+        int hp,
+        int attack,
+        int defense,
+        int initiative,
+        BugemonType type,
+        List<Attack> attacks,
+        String spritePath,
+        boolean isStarter,
+        boolean isBoss) {
+
     public static final int ATTACKS_COUNT = 3;
 
-    private final String id;
-    private final String name;
-    private final int hp;
-    private final int attack;
-    private final int defense;
-    private final int initiative;
-    private final BugemonType type;
-    private final List<Attack> attacks;
-    private final String spritePath;
-    private final boolean isStarter;
-    private final boolean isBoss;
+    public Bugemon {
+        Objects.requireNonNull(id);
+        Objects.requireNonNull(name);
+        Objects.requireNonNull(type);
+        Objects.requireNonNull(attacks);
+
+        // TODO: add checks for other fields (stats > 0, name/id not empty etc.)
+
+        attacks = List.copyOf(attacks);
+
+        if (attacks.size() != ATTACKS_COUNT) {
+            throw new InvalidAttackCountException(ATTACKS_COUNT, attacks.size());
+        }
+    }
 
     public Bugemon(String id, String name, int hp, int attack, int defense, int initiative, BugemonType type,
             List<Attack> attacks, String spritePath, boolean isStarter) {
         this(id, name, hp, attack, defense, initiative, type, attacks, spritePath, isStarter, false);
-    }
-
-    public Bugemon(String id, String name, int hp, int attack, int defense, int initiative, BugemonType type,
-            List<Attack> attacks, String spritePath, boolean isStarter, boolean isBoss) {
-        if (attacks.size() != ATTACKS_COUNT) {
-            throw new InvalidAttackCountException(ATTACKS_COUNT, attacks.size());
-        }
-
-        this.id = Objects.requireNonNull(id);
-        this.name = Objects.requireNonNull(name);
-        this.hp = hp;
-        this.attack = attack;
-        this.defense = defense;
-        this.initiative = initiative;
-        this.type = Objects.requireNonNull(type);
-        this.attacks = List.copyOf(attacks); // TODO: do we really need a copy here?
-        this.spritePath = spritePath;
-        this.isStarter = isStarter;
-        this.isBoss = isBoss;
-    }
-
-    public String getId() {
-        return this.id;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public int getHp() {
-        return this.hp;
-    }
-
-    public int getAttack() {
-        return this.attack;
-    }
-
-    public int getDefense() {
-        return this.defense;
-    }
-
-    public int getInitiative() {
-        return this.initiative;
-    }
-
-    public BugemonType getType() {
-        return this.type;
-    }
-
-    public List<Attack> getAttacks() {
-        return Collections.unmodifiableList(this.attacks);
-    }
-
-    public String getSpritePath() {
-        return this.spritePath;
-    }
-
-    public boolean isStarter() {
-        return this.isStarter;
-    }
-
-    public boolean isBoss() {
-        return this.isBoss;
     }
 
     @Override
