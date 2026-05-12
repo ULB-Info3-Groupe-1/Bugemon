ac6baa63ee9cd04bf90bd8a42967be31c87433e1
lulu <lucas.verbeiren@gmail.com>
2026-05-12 13:27:57 +0200

    bugemon: attacks count + cleanup
    Co-authored-by: Matteo Morbée <matteo.morbee@ulb.be>

--- src/main/java/ulb/models/bugemon/Bugemon.java
+++ src/main/java/ulb/models/bugemon/Bugemon.java
@@ -4,12 +4,16 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
+import ulb.models.bugemon.exceptions.InvalidAttackCountException;
+
 // TODO: check if we could make this become a record
 
 /**
  * Represents a Bugemon.
  */ 
 public class Bugemon {
+    public static final int ATTACKS_COUNT = 3;
+
     private final String id;
     private final String name;
     private final int hp;
@@ -29,6 +33,10 @@ public class Bugemon {
 
     public Bugemon(String id, String name, int hp, int attack, int defense, int initiative, BugemonType type,
             List<Attack> attacks, String spritePath, boolean isStarter, boolean isBoss) {
+        if (attacks.size() != ATTACKS_COUNT) {
+            throw new InvalidAttackCountException(ATTACKS_COUNT, attacks.size());
+        }
+
         this.id = Objects.requireNonNull(id);
         this.name = Objects.requireNonNull(name);
         this.hp = hp;
@@ -109,108 +117,4 @@ public class Bugemon {
         return String.format("%s [%s] HP:%d ATK:%d DEF:%d INIT:%d", this.name, this.type, this.hp, this.attack,
                 this.defense, this.initiative);
     }
-
-    // -----------------------------------------------------
-    // CODE BELOW SHOULD MOVE
-    // -----------------------------------------------------
-    //
-    // static List<Attack> validateAndCopyAttackList(List<Attack> attacks) {
-    // if (attacks.size() != ATTACKS_COUNT) {
-    // throw new InvalidAttackCountException(ATTACKS_COUNT, attacks.size());
-    // }
-    // return List.copyOf(attacks);
-    // }
-    //
-    // public void takeDamage(int damage) {
-    // this.healthComponent.decreaseHp(damage);
-    // }
-    //
-    // @Override
-    // public boolean isAlive() {
-    // return this.healthComponent.getHp() > 0;
-    // }
-    //
-    //
-    // // Getters and Setters
-    //
-    // /**
-    // * Get the name of the bugemon.
-    // *
-    // * @return (String) the name of the bugemon.
-    // */
-    //
-    // /** Clears all active modifiers on every stat component. */
-    // public void resetMalus() {
-    // this.healthComponent.resetMalus();
-    // this.attackComponent.resetMalus();
-    // this.defenseComponent.resetMalus();
-    // this.initiativeComponent.resetMalus();
-    // }
-    //
-    // @Override
-    // public int getLevel() {
-    // return this.levelComponent.getLevel();
-    // }
-    //
-    // public void restoreHp() {
-    // this.healthComponent.restoreHp();
-    // }
-    //
-    // public int getXp() {
-    // return this.levelComponent.getXp();
-    // }
-    //
-    // @Override
-    // public double getXpProgress() {
-    // return this.levelComponent.getXpProgress();
-    // }
-    //
-    // /**
-    // * Adds XP and returns the number of levels crossed.
-    // *
-    // * @return number of level-ups that just occurred
-    // */
-    // public int gainXp(int xp) {
-    // return this.levelComponent.addXp(xp);
-    // }
-    //
-    // public void applyUpgrade(Upgrade upgrade) {
-    // this.healthComponent.increaseMaxHp(upgrade.hp());
-    // this.attackComponent.increaseAttack(upgrade.attack());
-    // this.defenseComponent.increaseDefense(upgrade.defense());
-    // this.initiativeComponent.increaseInitiative(upgrade.initiative());
-    // }
-    //
-    // public void apply(Effect effect) {
-    // effect.applyTo(this);
-    // }
-    //
-    // public void apply(EffectStatModifier e) {
-    // Modifier m = (e.duration() == EffectDuration.ONE_TURN) ? new Modifier(e.modifier(), 1)
-    // : new Modifier(e.modifier());
-    // switch (e.stat()) {
-    // case HP -> this.healthComponent.addModifier(m);
-    // case ATTACK -> this.attackComponent.addModifier(m);
-    // case DEFENSE -> this.defenseComponent.addModifier(m);
-    // case INITIATIVE -> this.initiativeComponent.addModifier(m);
-    // default -> throw new IllegalArgumentException("unknown stat: " + e.stat());
-    // }
-    // }
-    //
-    // public void apply(EffectHeal e) {
-    // this.healthComponent.increaseHp(e.amount());
-    // }
-    //
-    // public void apply(EffectResetMalus e) {
-    // this.resetMalus();
-    // }
-    //
-    // public void kill() {
-    // this.takeDamage(this.getHp());
-    // }
-    //
-    // public List<String> getListAttacksId() {
-    // return this.attackList.stream().map(Attack::id).toList();
-    // }
-
 }
