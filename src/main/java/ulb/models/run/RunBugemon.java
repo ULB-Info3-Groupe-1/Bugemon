74d9979d8fe39abc0e6657ab50ac3e2c4670ada1
lulu <lucas.verbeiren@gmail.com>
2026-05-12 13:54:35 +0200

    RunBugemon
    Co-authored-by: Matteo Morbée <matteo.morbee@ulb.be>

---
+++ src/main/java/ulb/models/run/RunBugemon.java
@@ -0,0 +1,13 @@
+package ulb.models.run;
+
+import ulb.models.bugemon.PlayerBugemon;
+
+public class RunBugemon {
+        private final PlayerBugemon playerBugemon;
+        private int currentHp;
+
+        public RunBugemon(PlayerBugemon playerBugemon, int currentHp) {
+                this.playerBugemon = playerBugemon;
+                this.currentHp = currentHp;
+        }
+}
