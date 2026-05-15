5f3fcbbd6aaaec31f4aeb915f4c9ee6ad0cbb80a
lulu <lucas.verbeiren@gmail.com>
2026-05-15 15:53:42 +0200

    fancy DamageCalculator (FunctionalInterface for formulae)
    Lets us define the formulae used to compute the attack- and reduction-factors
    in a separate file like Configuration.

--- src/main/java/ulb/models/combat/DamageCalculator.java
+++ src/main/java/ulb/models/combat/DamageCalculator.java
@@ -3,6 +3,9 @@ package ulb.models.combat;
 import ulb.models.bugemon.Attack;
 
 public class DamageCalculator {
+    AttackFactorFormula attackFactorFormula;
+    ReductionFactorFormula reductionFactorFormula;
+
     public DamageCalculator() {
     }
 
@@ -10,7 +13,23 @@ public class DamageCalculator {
             CombatBugemon attacker,
             CombatBugemon defender,
             Attack attack) {
+        double baseDamage; // TODO: retrieve
+
+        double attackFactor = this.attackFactorFormula.evaluate(attacker.getEffectiveAttack());
+        double reductionFactor = this.reductionFactorFormula.evaluate(defender.getEffectiveDefense());
+
         // TODO: impl
+
         return 0;
+    }
+
+    @FunctionalInterface
+    public interface AttackFactorFormula {
+        double evaluate(int effectiveAttack);
+    }
+
+    @FunctionalInterface
+    public interface ReductionFactorFormula {
+        double evaluate(int effectiveDefense);
     }
 }
