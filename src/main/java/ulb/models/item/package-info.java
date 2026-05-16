5c9baa04abe6378a85b507352909c2265ce73be7
Ethan Van Ruyskensvelde <ethanvr3@gmail.com>
2026-05-16 14:36:09 +0200

    formatting

--- src/main/java/ulb/models/item/package-info.java
+++ src/main/java/ulb/models/item/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Item classes of the Bugemon game such as {@link ulb.models.item.Item} and {@link ulb.models.item.Inventory} for managing
- * the player's inventory.
+ * Item classes of the Bugemon game such as {@link ulb.models.item.Item} and {@link ulb.models.item.Inventory} for
+ * managing the player's inventory.
  */
 package ulb.models.item;
