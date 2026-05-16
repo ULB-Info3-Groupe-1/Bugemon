577f73653490055a1fdb3685c0e61fbe9ccb0a00
romainlief <romain.liefferinckx@ulb.be>
2026-05-16 13:35:13 +0200

    début strategy

---
+++ src/main/java/ulb/models/combat/strategy/MiniMaxStrategy.java
@@ -0,0 +1,18 @@
+package ulb.models.combat.strategy;
+
+import ulb.models.combat.turn.ActionCallback;
+import ulb.models.combat.utils.CombatContext;
+
+public class MiniMaxStrategy implements CombatStrategy {
+    public MiniMaxStrategy() {
+    }
+
+    @Override
+    public void chooseAction(CombatContext ctx, ActionCallback callback) {
+    }
+
+    @Override
+    public void chooseSwitch(CombatContext ctx, ActionCallback callback) {
+    }
+
+}
