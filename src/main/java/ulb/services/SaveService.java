735d74a3da4fb31283f9f483325b2b4320c40f4d
Matteo Morbée <matteo.morbee@ulb.be>
2026-05-19 16:47:51 +0200

    fix: TeamService refactor

--- src/main/java/ulb/services/SaveService.java
+++ src/main/java/ulb/services/SaveService.java
@@ -1,9 +1,6 @@
 package ulb.services;
 
 import ulb.models.player.PlayerState;
-import ulb.repositories.exceptions.TeamEmptyException;
-import ulb.repositories.exceptions.TeamNameAlreadyExistsException;
-import ulb.repositories.exceptions.TeamNameEmptyException;
 
 public class SaveService {
     private final PlayerService playerService;
@@ -11,32 +8,18 @@ public class SaveService {
     private final InventoryService inventoryService;
     private final TeamService teamService;
 
-    private PlayerState playerState;
-
     public SaveService(PlayerService playerService, BugemonService bugemonService, InventoryService inventoryService,
             TeamService teamService, PlayerState playerState) {
         this.playerService = playerService;
         this.bugemonService = bugemonService;
         this.inventoryService = inventoryService;
         this.teamService = teamService;
-        this.playerState = playerState;
     }
 
-    public void setPlayerState(PlayerState playerState) {
-        this.playerState = playerState;
-    }
-
-    public void save() {
+    public void save(PlayerState playerState) {
         this.playerService.save();
-        this.playerState.getActiveTeam().ifPresent(this.bugemonService::save);
-        this.inventoryService.save(this.playerState.getInventory());
-        this.playerState.getActiveTeam().ifPresent(team -> {
-            try {
-                this.teamService.saveTeam(team);
-            } catch (TeamNameAlreadyExistsException | TeamEmptyException | TeamNameEmptyException e) {
-                // TODO: check normal to catch error here ?
-                throw new IllegalStateException();
-            }
-        });
+        playerState.getActiveTeam().ifPresent(this.bugemonService::save);
+        this.inventoryService.save(playerState.getInventory());
+        playerState.getActiveTeam().ifPresent(this.teamService::save);
     }
 }
