3b3764da73caeb09e5e5fd6667c704ccefdcbd40
lulu <lucas.verbeiren@gmail.com>
2026-05-21 10:08:11 +0200

    FloorMapFactory imports

--- src/main/java/ulb/models/tower/utils/FloorMapFactory.java
+++ src/main/java/ulb/models/tower/utils/FloorMapFactory.java
@@ -4,13 +4,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Random;
-import java.util.Set;
 
 import ulb.models.tower.FloorMap;
 import ulb.models.tower.FloorMap.RoomPosition;
