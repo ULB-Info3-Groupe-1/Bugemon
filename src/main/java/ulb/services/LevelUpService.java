859dd8ea50a694889cbb592b7f2ba3328ab5bd3a
lulu <lucas.verbeiren@gmail.com>
2026-05-21 12:58:32 +0200

    update LevelUpService to use levelUpGenerator

--- src/main/java/ulb/services/LevelUpService.java
+++ src/main/java/ulb/services/LevelUpService.java
@@ -1,17 +1,23 @@
 package ulb.services;
 
 import java.util.List;
+import java.util.Random;
 
+import ulb.models.level_up.LevelUpGenerator;
 import ulb.models.level_up.Upgrade;
+import ulb.models.player.BonusStats;
 import ulb.models.run.RunBugemon;
 import ulb.repositories.BugemonRepository;
 
 public class LevelUpService {
     String playername;
     BugemonRepository bugemonRepository;
+    LevelUpGenerator levelUpGenerator;
 
-    public LevelUpService(String playername, BugemonRepository bugemonRepository) {
+    public LevelUpService(String playername, BugemonRepository bugemonRepository, Random random) {
+        this.playername = playername;
         this.bugemonRepository = bugemonRepository;
+        this.levelUpGenerator = new LevelUpGenerator(random);
     }
 
     public void applyLevelUp(RunBugemon bugemon, Upgrade upgrade) {
@@ -19,8 +25,7 @@ public class LevelUpService {
         this.bugemonRepository.save(bugemon.getPlayerBugemon().toDTO(this.playername));
     }
 
-    public List<Upgrade> generateLevelUpOptions() {
-        // TODO: impl
-        return List.of();
+    public List<BonusStats> generateLevelUpOptions() {
+        return this.levelUpGenerator.generateOptions();
     }
 }
