98240daaf4d9d699b8caa4fb4bf17a5d970d3095
Arkham591 <rraduloghon@gmail.com>
2026-05-22 16:04:16 +0200

    Delete unused classes

--- src/main/java/ulb/models/tower/exceptions/FloorLevelNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ulb.models.tower.exceptions;
-
-public class FloorLevelNotFoundException extends RuntimeException {
-    public FloorLevelNotFoundException(int level) {
-        super("The NO floor " + level + " does not exist in the tower.");
-    }
-}
