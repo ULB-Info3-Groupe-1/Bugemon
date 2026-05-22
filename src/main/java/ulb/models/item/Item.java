a0ab7494a4e9adf4e8e6d950d547b552cd49d96a
Philippe Brisbois <philippe.brisbois@ulb.be>
2026-05-22 17:30:23 +0200

    fix items db persistence

--- src/main/java/ulb/models/item/Item.java
+++ src/main/java/ulb/models/item/Item.java
@@ -1,9 +1,27 @@
 package ulb.models.item;
 
+import java.util.Objects;
+
 import com.google.gson.annotations.SerializedName;
 
 import ulb.models.effect.Effect;
 
 public record Item(String id, @SerializedName("nom") String name, String description,
         @SerializedName("categorie") ItemType type, @SerializedName("effet") Effect effect) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Item other)) {
+            return false;
+        }
+        return Objects.equals(this.id, other.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(this.id);
+    }
 }
